dc0f4d7faef4231d298d1dfaf0351ab19871eb01
kinasant <kinasant@yahoo.com>
2025-07-24 13:25:36 +0530

    Update

--- mpc_install.sh
+++ mpc_install.sh
@@ -1,16 +1,17 @@
 # download MP-SPDZ
-wget https://github.com/data61/MP-SPDZ/releases/download/v0.3.2/mp-spdz-0.3.2.tar.xz
+wget https://github.com/data61/MP-SPDZ/releases/download/v0.4.1/mp-spdz-0.4.1.tar.xz
 
-tar -xf mp-spdz-0.3.2.tar.xz
 
-rsync -av ./mpspdz/ ./mp-spdz-0.3.2/
+tar -xf mp-spdz-0.4.1.tar.xz
+
+rsync -av ./mpspdz/ ./mp-spdz-0.4.1/
 
 rm -r mpspdz
 
-mv -T mp-spdz-0.3.2 mpspdz
+mv -T mp-spdz-0.4.1 mpspdz
 
-rm -r mp-spdz-0.3.2
-rm -r mp-spdz-0.3.2.tar.xz
+rm -r mp-spdz-0.4.1
+rm -r mp-spdz-0.4.1.tar.xz
 
 # setup MP-SPDZ
 cd mpspdz
